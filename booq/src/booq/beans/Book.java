f5805aec5dbb8383241a39662eb2102bfabc22d9
Luke <Luke@Luke-PC>
2015-03-02 22:19:06 -0800

    Made it nice

--- booq/src/booq/beans/Book.java
+++ booq/src/booq/beans/Book.java
@@ -1,48 +1,31 @@
 package booq.beans;
 
 public class Book {
-  private double price; 
-  private String title; 
-  private String author; 
-  private int id; 
-  private int stock; 
-  public double getPrice() {
-	return price;
-}
-public void setPrice(double price) {
-	this.price = price;
-}
-public String getTitle() {
-	return title;
-}
-public void setTitle(String title) {
-	this.title = title;
-}
-public String getAuthor() {
-	return author;
-}
-public void setAuthor(String author) {
-	this.author = author;
-}
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public int getStock() {
-	return stock;
-}
-public void setStock(int stock) {
-	this.stock = stock;
-}
-public String getDescription() {
-	return description;
-}
-public void setDescription(String description) {
-	this.description = description;
-}
-String description; 
+	private double price; 
+	private String title; 
+	private String author; 
+	private int id; 
+	private int stock;
+	private int genreId;
+	private int genreName;
+	private String des;
   
+	public double getPrice()       { return price; }
+	public String getTitle()       { return title; }
+	public String getAuthor()      { return author; }
+	public int    getId()          { return id; }
+	public int    getStock()       { return stock; }
+	public int    getGenreId()     { return genreId; }
+	public int    getGenreName()   { return genreName; }
+	public String getDescription() { return des; }
+	
+	public void setPrice(double price)      { this.price = price; }
+	public void setTitle(String title)      { this.title = title; }
+	public void setAuthor(String author)    { this.author = author; }
+	public void setId(int id)               { this.id = id; }
+	public void setStock(int stock)         { this.stock = stock; }
+	public void setGenreId(int genreId)     { this.genreId = genreId; }
+	public void setGenreName(int genreName) { this.genreName = genreName; }
+	public void setDescription(String des)  { this.des = des; }  
   
 }
