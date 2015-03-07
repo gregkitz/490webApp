93e292e81c5123c2d9f77daea1208f949612b42e
Luke <Luke@Luke-PC>
2015-03-06 20:58:34 -0800

    finished bookqueries

--- booq/src/booq/beans/Book.java
+++ booq/src/booq/beans/Book.java
@@ -7,7 +7,7 @@ public class Book {
 	private int id; 
 	private int stock;
 	private int genreId;
-	private int genreName;
+	private String genreName;
 	private String des;
   
 	public double getPrice()       { return price; }
@@ -16,16 +16,16 @@ public class Book {
 	public int    getId()          { return id; }
 	public int    getStock()       { return stock; }
 	public int    getGenreId()     { return genreId; }
-	public int    getGenreName()   { return genreName; }
+	public String getGenreName()   { return genreName; }
 	public String getDescription() { return des; }
 	
-	public void setPrice(double price)      { this.price = price; }
-	public void setTitle(String title)      { this.title = title; }
-	public void setAuthor(String author)    { this.author = author; }
-	public void setId(int id)               { this.id = id; }
-	public void setStock(int stock)         { this.stock = stock; }
-	public void setGenreId(int genreId)     { this.genreId = genreId; }
-	public void setGenreName(int genreName) { this.genreName = genreName; }
-	public void setDescription(String des)  { this.des = des; }  
+	public void setPrice(double price)         { this.price = price; }
+	public void setTitle(String title)         { this.title = title; }
+	public void setAuthor(String author)       { this.author = author; }
+	public void setId(int id)                  { this.id = id; }
+	public void setStock(int stock)            { this.stock = stock; }
+	public void setGenreId(int genreId)        { this.genreId = genreId; }
+	public void setGenreName(String genreName) { this.genreName = genreName; }
+	public void setDescription(String des)     { this.des = des; }  
   
 }
--- booq/src/booq/model/BookQueries.java
+++ booq/src/booq/model/BookQueries.java
@@ -9,32 +9,60 @@ import java.util.ArrayList;
 import booq.beans.Book;
 
 public class BookQueries {
+	private static final String queryBase =
+		"select bookId, title, author, price, stock, "
+		+ "genreName, Book.genreId from Book inner join Genre on "
+		+ "Book.genreId = Genre.genreId where ";
+	
 	public static ArrayList<Book> searchBooks(DBConnectionPool connPool,
-											  String keyword) {
-		
+								  String keyword) {
+		String query = queryBase + "title like '%"	+ keyword.trim()
+				+ "%' or author like '%" + keyword.trim() + "%'";
+		return getBooks(connPool, query);
+	}
+	
+	public static ArrayList<Book> getBooksByGenre(DBConnectionPool connPool,
+		      int genreId) {
+		String query = queryBase + "Book.genreId = " + genreId;
+		return getBooks(connPool, query);
+	}
+	
+	public static Book getBook(DBConnectionPool connPool, int bookId) {
+		String query = queryBase + "bookId = "
+				+ bookId;
+		return getBooks(connPool, query).remove(0);
+	}
+	
+	private static ArrayList<Book> getBooks(DBConnectionPool connPool,
+		      String query) {
 		ArrayList<Book> books = new ArrayList<Book>();
 		try {
-			Connection conn = connPool.getConnection();
-			Statement s = conn.createStatement();
-			String query = "select bookId, title, author, price, stock, genreName from Book inner join Genre on Book.genreId = Genre.genreId"
-					+ " where title like '%" + keyword.trim() + "%'"
-					+ " or author like '%" + keyword.trim() + "%'";
-			ResultSet rs = s.executeQuery(query);
-			while (rs.next()) {
-	              Book book = new Book();
-	              book.setId(rs.getInt(1));
-	              book.setTitle(rs.getString(2));
-	              book.setAuthor(rs.getString(3));
-	              book.setPrice(rs.getDouble(4));
-	              book.setStock(rs.getInt(4));
-	              
-	              books.add(book);
-	              }
+			Connection conn = null;
+			try {
+				conn = connPool.getConnection();
+			} catch (Exception e) { e.printStackTrace(); }
 			
-		} catch (Exception e) { e.printStackTrace(); }
+			if (conn != null) {
+				Statement s = conn.createStatement();
+				ResultSet rs = s.executeQuery(query);
+				while (rs.next()) {
+		              Book book = new Book();
+		              book.setId(rs.getInt(1));
+		              book.setTitle(rs.getString(2));
+		              book.setAuthor(rs.getString(3));
+		              book.setPrice(rs.getDouble(4));
+		              book.setStock(rs.getInt(5));
+		              book.setGenreName(rs.getString(6));
+		              book.setGenreId(rs.getInt(7));
+		              books.add(book);
+				}
+				rs.close();
+				s.close();
+				connPool.closeAll();
+			}
+		} catch (SQLException e) { e.printStackTrace(); }
 		
-		
-		
-		
+		return books;
 	}
+	
 }
