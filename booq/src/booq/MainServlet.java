6f288dc7accde321e076628508fc5e6a31ab354c
Luke <Luke@Luke-PC>
2015-03-10 19:16:19 -0700

    bleh

--- booq/src/booq/MainServlet.java
+++ booq/src/booq/MainServlet.java
@@ -43,7 +43,7 @@ public class MainServlet extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
-	}
+	} 
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
