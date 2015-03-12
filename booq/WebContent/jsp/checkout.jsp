b917c778e8767666744004b8e532470cfa6f6539
Gregory <Gregory@d-10-156-17-195.dhcp4.washington.edu>
2015-03-12 15:33:06 -0700

    changed it back

--- booq/WebContent/jsp/checkout.jsp
+++ booq/WebContent/jsp/checkout.jsp
@@ -1,7 +1,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
      <%@page import="booq.beans.CartItem"%>
+     <%@page import="booq.beans.Order"%>
      <%@page import="java.util.ArrayList"%>
+     <%@page import="booq.model.DBConnectionPool"%>
+     <!--<jsp:useBean id="connPool" scope="application"
+  class="booq.model.DBConnectionPool"/>--> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -12,6 +16,19 @@
 <jsp:include page="navBar.jsp" flush="true"/>
 <br><br><br><br>
 <h2>Thank you for your purchase for: $<%=session.getAttribute("cartTotal") %></h2>
+<%String custIDstring = (String) session.getAttribute("customerIDstring");  %>
+
+<%
+ArrayList<CartItem> theShoppingCart = (ArrayList<CartItem>) session.getAttribute("cart"); 
+Order theOrder = new Order(); 
+theOrder.setItems(theShoppingCart); 
+theOrder.setCustId(Integer.parseInt(custIDstring)); 
+connPool = new DBConnectionPool();
+connPool.insertOrder(theOrder); 
+
+
+%>
+
 <%session.removeAttribute("cart") ;%><%
 ArrayList<CartItem> shoppingCart  = new ArrayList<CartItem>(); 
 if (session.getAttribute("cart") == null){
--- booq/src/DML.sql
+++ booq/src/DML.sql
@@ -23,23 +23,23 @@ insert into Genre(id, genreName) values
   (13,'Thriller');
 
 insert into Book(id, title, author, price, genreId, stock, picturePath) values
-  (1, 'Responsive Web Design', 'Ethan Marcotte', 44.99, 1, 5, 'pics/book1'),
-  (2, 'Adaptive Web Design', 'Aaron Guifennison', 24.99, 2, 1, 'pics/book2'),
-  (3, 'Chicago', 8.95, 3, 4, 'pics/book3'),
-  (4, 'The Red Book', 'Luke Selbeck', 8.95, 4, 7, 'pics/book4'),
-  (5, 'The Mayors Tounge', 'Henry XVI', 5.99, 5, 9, 'pics/book5'),
-  (6, 'The Greyish Brown Book', 'Greg Kitzmiller', 6.99, 6, 1, 'pics/book6'),
-  (7, 'Now Youre Thinking', 'Dallas van Ess', 8.95, 7, 3, 'pics/book7'),
-  (8, 'Pieces of Light', 'Luke Selbeck', 8.95, 8, 5, 'pics/book8'),
-  (9, 'Making short films', 'David Bowie', 8.95, 9, 7, 'pics/book9'),
+  (1, 'My Name Is Dallas', 'Greg Kitzmiller', 44.99, 1, 5, 'pics/book1'),
+  (2, 'Harry Potter and the Rolling Indiana Jones Goblet', 'David Bowie', 24.99, 2, 1, 'pics/book2'),
+  (3, 'Greg','Dallas van Ess', 8.95, 3, 4, 'pics/book3'),
+  (4, 'The Last Murderer', 'Luke Selbeck', 8.95, 4, 7, 'pics/book4'),
+  (5, 'Bubbles in my ***', 'Henry XVI', 5.99, 5, 9, 'pics/book5'),
+  (6, 'My Fantasies', 'Greg Kitzmiller', 6.99, 6, 1, 'pics/book6'),
+  (7, 'A Brief History of WoW', 'Dallas van Ess', 8.95, 7, 3, 'pics/book7'),
+  (8, 'A Day With Luke', 'Luke Selbeck', 8.95, 8, 5, 'pics/book8'),
+  (9, 'Who Dun It?', 'David Bowie', 8.95, 9, 7, 'pics/book9'),
   (10, 'What is Truth?', 'Greg Kitzmiller', 8.95, 3, 3, 'pics/book10'),
-  (11, 'Heal Your Mind', 'Dallas van Ess', 8.95, 11, 2, 'pics/book11'),
+  (11, 'Cowboys Past Curfew', 'Dallas van Ess', 8.95, 11, 2, 'pics/book11'),
   (12, 'Robots', 'Henry XVI', 8.95, 12, 2, 'pics/book12'),
   (13, 'Thriller', 'Michael Jackson', 8.95, 13, 1, 'pics/book13'),
-  (14, '50 things I lernt good', 'Greg Kitzmiller', 20.01, 11, 14, 'pics/book14'),
+  (14, '50 Layers of Skin', 'Greg Kitzmiller', 20.01, 11, 14, 'pics/book14'),
   (15, 'Working with Greg:  My Trials', 'Luke Selbeck', 5.00, 8, 7, 'pics/book15'),
   (16, 'Whos the Better Worker?', 'Luke Selbeck', 7.00, 3, 3, 'pics/book16'),
-  (17, 'That Time', 'David Bowie', 21.76, 3, 3, 'pics/book17'),
+  (17, 'That Time I did the thing', 'David Bowie', 21.76, 3, 3, 'pics/book17'),
   (18, 'Etymology of Linux', 'Dallas van Ess', 1.99, 10, 8, 'pics/book18');
 
 update Book set
@@ -87,10 +87,6 @@ insert into Ratings(bookId, customerId, ratingDate, rating) values
 update Ratings SET description = 
   "I was blown away by this book"
   where bookId = 1 and customerId = 1;
-  
-  update Ratings SET description = 
-  "I was blown away by this book"
-  where bookId = 2 and customerId = 1;
 
 insert into MainOrder (customerId, orderDate) values (1, now());
 insert into CartItem (bookId, quantity) values (2, 1);
